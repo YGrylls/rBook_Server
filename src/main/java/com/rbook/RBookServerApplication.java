d562cc27963586603970cdd7c278840c2a68e867
YGrylls <910357107@qq.com>
2018-12-31 16:11:36 +0800

    initial commit

---
+++ src/main/java/com/rbook/RBookServerApplication.java
@@ -0,0 +1,14 @@
+package com.rbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RBookServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RBookServerApplication.class, args);
+	}
+
+}
+
